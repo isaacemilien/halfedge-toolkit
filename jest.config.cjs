af8f58a286740f38fe449ffeabe10c74495f6212
Isaac Emilien <isaac.emilien@gmail.com>
2025-05-28 15:11:51 +0100

    Initial commit

---
+++ jest.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  },
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__tests__/**/*.ts',
+    '**/?(*.)+(spec|test).ts'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/*.test.ts'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
+};
